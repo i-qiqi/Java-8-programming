47bfcc31e1fb8c64abd98b396352d9d969624e21
biqi zhu <biqi.zhu@sap.com>
2019-09-23 17:56:53 +0800

    Joseph

--- src/main/java/graphsearch/MinPath.java
+++ src/main/java/graphsearch/MinPath.java
@@ -12,75 +12,75 @@ public class MinPath {
             this.distance = d;
         }
     }
-    public int minPathLength(int[][] matrix, int end_x , int end_y){
-      int[][] directions = {{0,1},{0,-1},{1,0}, {-1,0}};
-      int m = matrix.length, n = matrix[0].length;
-      Queue<Pair<Integer,Integer>> q = new LinkedList<>();
-      int[][] visited = new int[m][n];
-      q.offer(new Pair<>(0, 0));
-      int distance = 0;
-      while(!q.isEmpty()){
-        int size = q.size();
-        //visit
-        distance++;
-        while(size-- > 0){//size 为每一层的节点数量 //双while法
-            Pair<Integer, Integer> cur = q.poll();
-            int x = cur.getKey(), y = cur.getValue();
-            visited[x][y] = 1;
-            for(int[] d : directions){
-                int next_x = x+d[0];
-                int next_y = y+d[1];
-                if(next_x >= 0 && next_x < m && next_y >= 0 && next_y < n
-                            && matrix[next_x][next_y] == 1 && visited[next_x][next_y] == 0){
-                        if(next_x == end_x && next_y == end_y) return distance;
-                        q.offer(new Pair<>(next_x, next_y));
-                }
-            }
-        }
-      }
+//    public int minPathLength(int[][] matrix, int end_x , int end_y){
+//      int[][] directions = {{0,1},{0,-1},{1,0}, {-1,0}};
+//      int m = matrix.length, n = matrix[0].length;
+//      Queue<Pair<Integer,Integer>> q = new LinkedList<>();
+//      int[][] visited = new int[m][n];
+//      q.offer(new Pair<>(0, 0));
+//      int distance = 0;
+//      while(!q.isEmpty()){
+//        int size = q.size();
+//        //visit
+//        distance++;
+//        while(size-- > 0){//size 为每一层的节点数量 //双while法
+//            Pair<Integer, Integer> cur = q.poll();
+//            int x = cur.getKey(), y = cur.getValue();
+//            visited[x][y] = 1;
+//            for(int[] d : directions){
+//                int next_x = x+d[0];
+//                int next_y = y+d[1];
+//                if(next_x >= 0 && next_x < m && next_y >= 0 && next_y < n
+//                            && matrix[next_x][next_y] == 1 && visited[next_x][next_y] == 0){
+//                        if(next_x == end_x && next_y == end_y) return distance;
+//                        q.offer(new Pair<>(next_x, next_y));
+//                }
+//            }
+//        }
+//      }
+//
+//      return -1;
+//
+//    }
+//    public int minPathLength_1(int[][] matrix, int end_x , int end_y){
+//        int[][] directions = {{0,1},{0,-1},{1,0}, {-1,0}};
+//        int m = matrix.length, n = matrix[0].length;
+//        Queue<Node> q = new LinkedList<>();
+//        int[][] visited = new int[m][n];
+//        q.offer(new Node(0, 0 , 0));
+//        while(!q.isEmpty()){
+//          Node cur = q.poll();
+//          int x = cur.x, y = cur.y, distance = cur.distance;
+//          visited[x][y] = 1;
+//          for(int[] d : directions){
+//              int next_x = x+d[0];
+//              int next_y = y+d[1];
+//              int next_d = distance+1;
+//              if(next_x >= 0 && next_x < m && next_y >= 0 && next_y < n
+//                          && matrix[next_x][next_y] == 1 && visited[next_x][next_y] == 0){
+//                      if(next_x == end_x && next_y == end_y) return next_d;
+//                      q.offer(new Node(next_x, next_y, next_d));
+//              }
+//          }
+//        }
+//
+//        return -1;
+//
+//      }
 
-      return -1;
 
-    }
-    public int minPathLength_1(int[][] matrix, int end_x , int end_y){
-        int[][] directions = {{0,1},{0,-1},{1,0}, {-1,0}};
-        int m = matrix.length, n = matrix[0].length;
-        Queue<Node> q = new LinkedList<>();
-        int[][] visited = new int[m][n];
-        q.offer(new Node(0, 0 , 0));
-        while(!q.isEmpty()){
-          Node cur = q.poll();
-          int x = cur.x, y = cur.y, distance = cur.distance;
-          visited[x][y] = 1;
-          for(int[] d : directions){
-              int next_x = x+d[0];
-              int next_y = y+d[1];
-              int next_d = distance+1;
-              if(next_x >= 0 && next_x < m && next_y >= 0 && next_y < n
-                          && matrix[next_x][next_y] == 1 && visited[next_x][next_y] == 0){
-                      if(next_x == end_x && next_y == end_y) return next_d;
-                      q.offer(new Node(next_x, next_y, next_d));
-              }
-          }
-        }
-  
-        return -1;
-  
-      }
-
-
-    public static void main(String[] args){
-        int[][] matrix = {
-            {1,1,0,1},
-            {1,0,1,1},             
-            {1,1,1,0},
-            {1,0,1,1}
-        };
-
-        int ans = 0;
-
-        MinPath minPath = new MinPath();
-        System.out.println(minPath.minPathLength(matrix, 0, 3));  
-        System.out.println(minPath.minPathLength_1(matrix, 0, 3));
-    }
+//    public static void main(String[] args){
+//        int[][] matrix = {
+//            {1,1,0,1},
+//            {1,0,1,1},
+//            {1,1,1,0},
+//            {1,0,1,1}
+//        };
+//
+//        int ans = 0;
+//
+//        MinPath minPath = new MinPath();
+//        System.out.println(minPath.minPathLength(matrix, 0, 3));
+//        System.out.println(minPath.minPathLength_1(matrix, 0, 3));
+//    }
 }
---
+++ src/main/java/swordoffer/JosephGame.java
@@ -0,0 +1,30 @@
+package swordoffer;
+
+import java.util.ArrayList;
+
+public class JosephGame {
+    public static  void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = 7 , m = 3;
+        for(int i = 1; i <= n; i++) list.add(i);
+        int k = 0;
+        while (n > 0){
+            k = (k + m - 1) % n;
+            System.out.println(list.get(k));
+            list.remove(k);
+            n--;
+        }
+        System.out.println("----");
+        Joseph_math(n, m);
+    }
+
+    public static  void Joseph_math(int n , int m){
+        int  i, s = 0;
+        for (i = 2; i <= n; i++)
+        {
+            s = (s + m) % i;
+            System.out.println(s);
+        }
+        System.out.println(s+1);
+    }
+}
